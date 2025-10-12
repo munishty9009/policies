1ed7218f0f48d7086e1737fd6ec47e58912f6169
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2025-10-12 18:06:59 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+# Get subscription info
+data "azurerm_subscription" "primary" {}
+
+# Resource group (scope)
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = "East US"
+}
+
+# Create custom role
+resource "azurerm_role_definition" "custom_role" {
+  name        = "CustomVMReader"
+  scope       = data.azurerm_subscription.primary.id
+  description = "Custom role: read-only access to VMs"
+  assignable_scopes = [
+    data.azurerm_subscription.primary.id
+  ]
+
+  permissions {
+    actions = [
+      "Microsoft.Compute/virtualMachines/read",
+      "Microsoft.Network/networkInterfaces/read",
+      "Microsoft.Network/publicIPAddresses/read"
+    ]
+    not_actions = []
+  }
+}
+
+# Assign custom role to service principal
+resource "azurerm_role_assignment" "sp_role" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = azurerm_role_definition.custom_role.name
+  principal_id         = var.sp_object_id
+}
