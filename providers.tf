855e7275877cb37177b853db797f1a23217a1a75
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2025-10-14 22:54:21 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "MyFirstRGfromTerraform"
-    storage_account_name  = "mystorageaccount1990"
-    container_name        = "state-files"
-    key                   = "ApplyPolicies"
-  }
 }
-
+}
 
 terraform {
   required_version = ">= 1.5.0"
